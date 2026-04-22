1df159e902dda2e39f60c1cdb43bd3025c14b205
ahmadwafii11 <wafiahmad950@gmail.com>
2026-04-22 12:19:02 +0700

    feat: Navbar

---
+++ frontend/vite-project/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+
+function Navbar() {
+    const [collapsed, setCollapsed] = useState(false)
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Navbar
